d4b6cee9da2cd93fa0c9dfe23dde90e3e8ed3247
Marlon Gayle <marlon.gayle@live.com>
2021-04-04 18:55:27 -0500

    Create custom exceptions

---
+++ Net5WebTemplate.Application/Common/Exceptions/DeleteFailureException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Net5WebTemplate.Application.Common.Exceptions
+{
+    public class DeleteFailureException : Exception
+    {
+        public DeleteFailureException(string name, object key, string message)
+            : base($"Deletion of entity \"{name}\" ({key}) failed. {message}")
+        {
+        }
+    }
+}
