bbedacff21add9180757ae4bafb0ed643b57a886
Marlon Gayle <marlon.gayle@live.com>
2021-04-04 12:19:31 -0500

    Configure Identity Server

--- Net5WebTemplate.Infrastructure/DependencyInjection.cs
+++ Net5WebTemplate.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Net5WebTemplate.Infrastructure.Identity;
 
 namespace Net5WebTemplate.Infrastructure
 {
@@ -8,6 +11,18 @@ namespace Net5WebTemplate.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
         {
+            services.AddDbContext<ApplicationIdentityDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("Net5WebTemplateDbConnection")));
+
+            services.AddDefaultIdentity<ApplicationUser>()
+                .AddEntityFrameworkStores<ApplicationIdentityDbContext>();
+
+            services.AddIdentityServer()
+                .AddApiAuthorization<ApplicationUser, ApplicationIdentityDbContext>();
+
+            services.AddAuthentication()
+                .AddIdentityServerJwt();
+
             return services;
         }
     }
---
+++ Net5WebTemplate.Infrastructure/Identity/ApplicationIdentityDbContext.cs
@@ -0,0 +1,15 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Net5WebTemplate.Infrastructure.Identity
+{
+    public class ApplicationIdentityDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationIdentityDbContext(DbContextOptions<ApplicationIdentityDbContext> options, 
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
