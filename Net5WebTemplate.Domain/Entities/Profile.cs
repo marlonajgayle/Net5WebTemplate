112401c172c707b21f18cd906c1ddb8e4a704211
Marlon Gayle <marlon.gayle@live.com>
2021-06-04 19:28:32 -0500

    Create Profile entity along with Address ValueObject

---
+++ Net5WebTemplate.Domain/Entities/Profile.cs
@@ -0,0 +1,11 @@
+﻿using Net5WebTemplate.Domain.ValueObjects;
+
+namespace Net5WebTemplate.Domain.Entities
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public Address Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
