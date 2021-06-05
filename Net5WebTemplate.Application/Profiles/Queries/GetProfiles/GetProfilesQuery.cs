957e9e211b8bcfa7396f799b030eaa7b43977c27
Marlon Gayle <marlon.gayle@live.com>
2021-06-04 21:56:26 -0500

    Create Get Profiles query

---
+++ Net5WebTemplate.Application/Profiles/Queries/GetProfiles/GetProfilesQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System.Collections.Generic;
+
+namespace Net5WebTemplate.Application.Profiles.Queries.GetProfiles
+{
+    public class GetProfilesQuery : IRequest<List<ProfileDto>>
+    {
+
+    }
+}
