2ddaab3ebdc157b928801aa2f9aa368d0f982fd4
Demar Johnson <DJohnson@realdecoy.com>
2021-06-07 00:21:34 -0500

    unit test

--- Net5WebTemplate.UnitTests/Application/Account/Command/CreateUserAccountTest.cs
+++ Net5WebTemplate.UnitTests/Application/Account/Command/CreateUserAccountTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Net5WebTemplate.Application.Common.Interfaces;
+using Net5WebTemplate.Api.Controllers.Version1
 
 namespace Net5WebTemplate.UnitTests.Application.Account.Command
 {
@@ -17,10 +18,10 @@ namespace Net5WebTemplate.UnitTests.Application.Account.Command
             var email = "";
             var password = "";
             var mock = new Mock<IUserManager>();
-            mock.Setup(p => p.CreateUserAsync(1)).Returns("Jignesh");
-            HomeController home = new HomeController(mock.Object);
-            string result = home.GetNameById(1);
-            Assert.AreEqual("Jignesh", result);
+            mock.Setup(p => p.CreateUserAsync(1)).Returns("confirm");
+            AccountController account = new AccountController(mock.Object);
+            string result = account.Create(RegisterRequest);
+            Assert.AreEqual("confirm", result);
         }
     }
 }
