25469584c2ee7635bdbb09ea674606f649325add
Demar Johnson <DJohnson@realdecoy.com>
2021-06-08 21:27:41 -0500

    unit test for refreshToken

--- Net5WebTemplate.UnitTests/Application/Auth/Command/LoginTest.cs
+++ Net5WebTemplate.UnitTests/Application/Auth/Command/LoginTest.cs
@@ -1,21 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System.Text;
 using Moq;
 using System.Threading.Tasks;
 using Net5WebTemplate.Application.Common.Interfaces;
 using Net5WebTemplate.Api.Controllers.Version1
 using Xunit.Sdk;
+using NUnit.Framework.Internal;
 
 namespace Net5WebTemplate.UnitTests.Application.Auth.Command
 {
-
     class LoginTest
     {
         private readonly ISignInManager _signInManager;
         [TestMethod]
-        public void CreateUserAccount()
+        public void Login() 
         {
             var email = "test@email.com";
             var password = "Password";
---
+++ Net5WebTemplate.UnitTests/Application/Auth/Command/RefreshTokenTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using Moq;
+using System.Threading.Tasks;
+using Net5WebTemplate.Application.Common.Interfaces;
+using Net5WebTemplate.Api.Controllers.Version1
+using Xunit.Sdk;
+using NUnit.Framework.Internal;
+
+namespace Net5WebTemplate.UnitTests.Application.Auth.Command
+{
+    class RefreshTokenTest
+    {
+        private readonly IJwtSecurityTokenManager _jwtSecurityTokenManager;
+
+        [TestMethod]
+        public void RefreshTokenTest()
+        {
+            var token = "token";
+            var refreshToken = "new token";
+            var cancellationToken = "canceltoken";
+            var mock = new Mock<IJwtSecurityTokenManager>();
+            mock.Setup(p => p.RefreshTokenAsync(token, refreshToken, cancellationToken)).Returns("<TokenResult>result"); ;
+            AuthController auth = new AuthController(mock.Object);
+            string result = auth.RefreshToken(RefreshTokenRequest request);
+            Assert.AreEqual("result", result);
+
+        }
+    }
+}
